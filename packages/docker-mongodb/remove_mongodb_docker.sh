bc59e46eea463f22c30415145b32250634bcb258
ajay <ajmatharu@gmail.com>
2019-04-15 17:27:17 -0700

    changes in script file to make it work in windows

--- packages/docker-mongodb/remove_mongodb_docker.sh
+++ packages/docker-mongodb/remove_mongodb_docker.sh
@@ -4,6 +4,7 @@ set -o errexit
 # change to script's directory
 cd "$(dirname "$0")"
 if [ "$(docker ps -q -f name=eosio-mongodb)" ]; then
-  docker stop eosio-mongodb && rm -rf data && mkdir data
+  docker stop eosio-mongodb
+  docker volume rm --force mongodata
 fi
 
