14c159997d1498083cde434ade4a86dddd5a760b
ajay matharu <ajmatharu@gmail.com>
2019-05-15 11:11:42 +0800

    Fogbugz 3386 - Create api to updateauth in blockchain

--- packages/api-rpc/dist/index.js
+++ packages/api-rpc/dist/index.js
@@ -8,10 +8,12 @@ var create_account_1 = __importDefault(require("./api/create_account"));
 var push_action_1 = __importDefault(require("./api/push_action"));
 var get_table_rows_1 = __importDefault(require("./api/get_table_rows"));
 var get_account_details_1 = __importDefault(require("./api/get_account_details"));
+var update_auth_1 = __importDefault(require("./api/update_auth"));
 exports.default = {
     get_info: get_info_1.default,
     create_account: create_account_1.default,
     push_action: push_action_1.default,
     get_table_rows: get_table_rows_1.default,
-    get_account_details: get_account_details_1.default
+    get_account_details: get_account_details_1.default,
+    update_auth: update_auth_1.default
 };
