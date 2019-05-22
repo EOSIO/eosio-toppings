ce3c9e73425bc22cf5ed38f9d7d73e1691df5795
ajay matharu <ajmatharu@gmail.com>
2019-05-22 15:13:39 +0800

    - Removed continue_blockchain.sh as it is not being used - Add stop_blockchain.sh to gracefully stop nodeos - Update remove_mogodb_docker and remove_eosio_docker to first stop the docker and then remove it to avoid any issue with docker - Update start_mongodb_docker and start_eosio_docker to create the volume if it doesnt exists (it doesnt remove the volume anymore) and also removed -rm flag from docker run to retain the container

--- packages/docker-eosio-nodeos/remove_eosio_docker.sh
+++ packages/docker-eosio-nodeos/remove_eosio_docker.sh
@@ -11,11 +11,18 @@ fi
 
 # change to script's directory
 cd "$(dirname "$0")"
+
+echo "waiting for docker to stop"
 # stop the container if the container is running
 if [ "$(docker ps -q -f name=^$NODEOS_CONTAINER_NAME$)" ]; then
   docker stop $NODEOS_CONTAINER_NAME
   sleep 10
 fi
+
+if [ "$(docker ps -q -a -f name=^$NODEOS_CONTAINER_NAME$)" ]; then
+  docker rm $NODEOS_CONTAINER_NAME
+fi
+
 # also remove blockchain volume if its exists
 if [ "$(docker volume ls --format '{{.Name}}' -f name=^$NODEOS_VOLUME_NAME$)" ]; then
     docker volume rm --force $NODEOS_VOLUME_NAME
--- packages/docker-mongodb/start_mongodb_docker.sh
+++ packages/docker-mongodb/start_mongodb_docker.sh
@@ -20,16 +20,11 @@ fi
 # check if mongodb container is running
 if [ ! "$(docker ps -q -f name=^$MONGODB_CONTAINER_NAME$)" ]; then
   # check if the volume exists 
-  if [ "$(docker volume ls --format '{{.Name}}' -f name=^$MONGODB_VOLUME_NAME$)" ]; then
-    echo "mongodb docker is not running, but mongodb volume exists"
-    echo "cleaning mongodb volume"
-    # remove the volume because docker is not running but the volume exists
-    docker volume rm --force $MONGODB_VOLUME_NAME
-    sleep 10
+  if [ ! "$(docker volume ls --format '{{.Name}}' -f name=^$MONGODB_VOLUME_NAME$)" ]; then
+    # create volume
+    docker volume create --name=$MONGODB_VOLUME_NAME
   fi
-  # create fresh volume and start docker
-  docker volume create --name=$MONGODB_VOLUME_NAME
-  docker run -d --rm -p $MONGODB_PORT:$MONGODB_PORT --name $MONGODB_CONTAINER_NAME -v $MONGODB_VOLUME_NAME:/data/db mongo --port $MONGODB_PORT
+  docker run -d -p $MONGODB_PORT:$MONGODB_PORT --name $MONGODB_CONTAINER_NAME -v $MONGODB_VOLUME_NAME:/data/db mongo --port $MONGODB_PORT
 else
   echo "docker already running"
 fi
