6d3b36b89231d2b3c59ae017548ac5c57e1e7930
varshajnagaraja <varshajn99@gmail.com>
2019-09-12 17:22:00 +0800

    API's to stake/unstake resources

---
+++ packages/api-rpc/src/api/sell_ram.ts
@@ -0,0 +1,44 @@
+import { Api, JsonRpc, Serialize } from 'eosjs';
+import JsSignatureProvider from 'eosjs/dist/eosjs-jssig';
+import { TextDecoder, TextEncoder } from 'text-encoding';
+
+const sell_ram = async (query: {
+  endpoint: string,
+  private_key: string,
+  actor: string,
+  permission: string,
+  quantity: any
+}) => {
+  try{
+    let { endpoint, private_key, actor, permission, quantity = '10000' } = query;
+    const rpc = new JsonRpc(endpoint);
+    const signatureProvider = new JsSignatureProvider([private_key]);
+    const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+    const result = await api.transact({
+      actions: [{
+        account: "eosio",
+        name: "sellram",
+        authorization: [{
+          actor: actor,
+          permission: permission,
+        }],
+        data: {
+          account: actor,
+          bytes: quantity
+        }
+      }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 30,
+    });
+
+    return result;
+
+  }catch(e){
+    console.log('Caught exception: ' + e);
+    throw(e);
+  }
+}
+
+export default sell_ram;
---
+++ packages/api-rpc/src/api/unstake_net.ts
@@ -0,0 +1,48 @@
+import { Api, JsonRpc, Serialize } from 'eosjs';
+import JsSignatureProvider from 'eosjs/dist/eosjs-jssig';
+import { TextDecoder, TextEncoder } from 'text-encoding';
+
+const unstake_net = async (query: {
+  endpoint: string,
+  private_key: string,
+  actor: string,
+  permission: string,
+  quantity: any
+}) => {
+  try{
+    let { endpoint, private_key, actor, permission, quantity = '1.0000 TNT' } = query;
+    console.log("query ", query)
+    const rpc = new JsonRpc(endpoint);
+    const signatureProvider = new JsSignatureProvider([private_key]);
+    const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+    const result = await api.transact({
+      actions: [{
+        account: "eosio",
+        name: "undelegatebw",
+        authorization: [{
+          actor: actor,
+          permission: permission,
+        }],
+        data: {
+          from: actor,
+          receiver: actor,
+          unstake_net_quantity: quantity,
+          unstake_cpu_quantity: '0.0000 TNT',
+          transfer: false,
+        }
+      }]
+    }, {
+      blocksBehind: 3,
+      expireSeconds: 30,
+    });
+
+    return result;
+
+  }catch(e){
+    console.log('Caught exception: ' + e);
+    throw(e);
+  }
+}
+
+export default unstake_net;
