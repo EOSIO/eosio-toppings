cea1007f854c035f069948a6925a3694d869d0a8
varshajnagaraja <varshajn99@gmail.com>
2019-09-16 09:56:41 +0800

    Remove inline actions (#102)
    * API's to stake/unstake resources
    
    * Changes to create account initial values
    
    * Fogbugz 3813: remove inline actions

--- packages/api-postgres-plugin/api/get_actions.js
+++ packages/api-postgres-plugin/api/get_actions.js
@@ -7,8 +7,8 @@ const get_actions = async (query) => {
     SELECT at.transaction_id, at.action_ordinal, at.act_account, at.act_name, at.act_data, at.timestamp, at.block_num,
     (SELECT ata.actor FROM chain.action_trace_authorization ata WHERE ata.block_num = at.block_num AND ata.transaction_id = at.transaction_id AND ata.action_ordinal = at.action_ordinal LIMIT 1),
     (SELECT ata.permission FROM chain.action_trace_authorization ata WHERE ata.block_num = at.block_num AND ata.transaction_id = at.transaction_id AND ata.action_ordinal = at.action_ordinal LIMIT 1)
-    FROM chain.action_trace AT
-    ${(account_name !== undefined) ? `WHERE at.act_account = '${account_name}'`:  '' }
+    FROM chain.action_trace at
+    WHERE at.creator_action_ordinal = 0 ${(account_name !== undefined) ? `AND at.act_account = '${account_name}'`:  '' }
     ORDER BY at.block_num DESC
     LIMIT ${(records_count !== undefined) ? parseInt(records_count) :  100}`;
     
