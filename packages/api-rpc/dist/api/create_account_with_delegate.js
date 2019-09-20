fafd016544f6f1dbdb228c46bba4831e4f5db5cc
Gordon Yip <52950648+gandalf-apprentice@users.noreply.github.com>
2019-09-20 14:09:20 +0800

    Add deploy contract api (#103)
    * add deploy_contract
    
    * deploy contract endpoint
    
    * change to index of api-rpc
    
    * tsc compile
    
    * clean up

--- packages/api-rpc/dist/api/create_account_with_delegate.js
+++ packages/api-rpc/dist/api/create_account_with_delegate.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -37,13 +38,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var eosjs_1 = require("eosjs");
 var eosjs_jssig_1 = __importDefault(require("eosjs/dist/eosjs-jssig"));
 var text_encoding_1 = require("text-encoding");
 var fetch = require('node-fetch');
-var create_account_with_delegate = function (query) { return __awaiter(_this, void 0, void 0, function () {
+var create_account_with_delegate = function (query) { return __awaiter(void 0, void 0, void 0, function () {
     var endpoint, creator_private_key, creator_account_name, creator_account_permission, new_account_name, new_account_owner_key, new_account_active_key, initial_ram_bytes, initial_net_cpu_quantity, rpc, signatureProvider, api, result, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
@@ -111,7 +111,7 @@ var create_account_with_delegate = function (query) { return __awaiter(_this, vo
                                     receiver: new_account_name,
                                     stake_net_quantity: initial_net_cpu_quantity,
                                     stake_cpu_quantity: initial_net_cpu_quantity,
-                                    transfer: false,
+                                    transfer: true,
                                 }
                             }]
                     }, {
--- packages/api-rpc/dist/api/stake_cpu.js
+++ packages/api-rpc/dist/api/stake_cpu.js
@@ -1,9 +1,10 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -37,13 +38,12 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var eosjs_1 = require("eosjs");
 var eosjs_jssig_1 = __importDefault(require("eosjs/dist/eosjs-jssig"));
 var text_encoding_1 = require("text-encoding");
 var fetch = require('node-fetch');
-var stake_cpu = function (query) { return __awaiter(_this, void 0, void 0, function () {
+var stake_cpu = function (query) { return __awaiter(void 0, void 0, void 0, function () {
     var endpoint, private_key, actor, permission, _a, quantity, rpc, signatureProvider, api, result, e_1;
     return __generator(this, function (_b) {
         switch (_b.label) {
