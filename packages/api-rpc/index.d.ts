e7413004c499525a5a6946577cfeadee65680b25
Matthew Kelly <matthew.kelly@block.one>
2019-09-25 18:04:11 +0800

    Added methods to TS declaration file for: - stake_cpu - unstake_cpu - buy_ram - sell_ram

--- packages/api-rpc/index.d.ts
+++ packages/api-rpc/index.d.ts
@@ -26,10 +26,46 @@ declare namespace Rpc {
     }
   }
 
+  interface StakeCpuQuery {
+    endpoint: string,
+    private_key: string,
+    actor: string,
+    permission: string,
+    quantity: any
+  }
+
+  interface UnstakeCpuQuery {
+    endpoint: string,
+    private_key: string,
+    actor: string,
+    permission: string,
+    quantity: any
+  }
+
+  interface BuyRamQuery {
+    endpoint: string,
+    private_key: string,
+    actor: string,
+    permission: string,
+    quantity: any
+  }
+
+  interface SellRamQuery {
+    endpoint: string,
+    private_key: string,
+    actor: string,
+    permission: string,
+    quantity: any
+  }
+
   function get_info(query: GetInfoQuery) : Promise<any>;
   function get_block(query: GetBlockQuery) : Promise<any>;
   function push_action(query: PushActionQuery) : Promise<any>;
   function deploy_contract(query: DeployContractQuery): Promise<any>;
+  function stake_cpu(query: StakeCpuQuery): Promise<any>;
+  function unstake_cpu(query: UnstakeCpuQuery): Promise<any>;  
+  function buy_ram(query: StakeCpuQuery): Promise<any>;
+  function sell_ram(query: SellRamQuery): Promise<any>;
 }
 
 export = Rpc;
