f1a5d9da7d87265c34250f0883ba4142c7be1ffd
varshajnagaraja <varshajn99@gmail.com>
2019-09-27 16:55:48 +0800

    JS files compiled

--- packages/api-rpc/dist/api/get_info.js
+++ packages/api-rpc/dist/api/get_info.js
@@ -1,10 +1,9 @@
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
@@ -35,10 +34,11 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var eosjs_1 = require("eosjs");
 var fetch = require('node-fetch');
-var get_info = function (query) { return __awaiter(void 0, void 0, void 0, function () {
+var get_info = function (query) { return __awaiter(_this, void 0, void 0, function () {
     var endpoint, rpc, response, e_1;
     return __generator(this, function (_a) {
         switch (_a.label) {
