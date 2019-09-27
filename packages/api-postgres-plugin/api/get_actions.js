5eecd5fd7081945ece548a5de65d8a1c00ee381a
varshajnagaraja <varshajn99@gmail.com>
2019-09-27 11:50:24 +0800

    Pg issue fix (#106)
    * Pg issue in prod fix
    
    * Fix issue with testnet
    
    * Action details change
    
    * Added declaration for connect db function
    
    * action query change
    
    * Change back frompool.connect to pool.query

--- packages/api-postgres-plugin/api/get_actions.js
+++ packages/api-postgres-plugin/api/get_actions.js
@@ -4,25 +4,27 @@ const get_actions = async (query) => {
   try{
     let { account_name, records_count, fetch_failed_action } = query;
 
-    //ORDER BY at.receipt_global_sequence DESC this has to be added back
-
     let query_gen = `
     SELECT at.transaction_id, at.action_ordinal, at.act_account, at.act_name, at.act_data, at.timestamp, at.block_num,
     (SELECT ata.actor FROM chain.action_trace_authorization ata WHERE ata.block_num = at.block_num AND ata.transaction_id = at.transaction_id AND ata.action_ordinal = at.action_ordinal LIMIT 1),
     (SELECT ata.permission FROM chain.action_trace_authorization ata WHERE ata.block_num = at.block_num AND ata.transaction_id = at.transaction_id AND ata.action_ordinal = at.action_ordinal LIMIT 1)
     FROM chain.action_trace at
-    WHERE at.creator_action_ordinal = 0 ${(account_name !== undefined) ? `AND at.act_account = '${account_name}'`:  '' }   
-    ORDER BY at.block_num DESC 
+    WHERE at.creator_action_ordinal = 0 ${(account_name !== undefined) ? `AND at.act_account = '${account_name}'`:  '' }  
+    ORDER BY at.block_num DESC  
     LIMIT ${(records_count !== undefined) ? parseInt(records_count) :  100}`;
 
-    let pool = db.getPool();
-    const client = await pool.connect();
-    try {
-      const res = await client.query(query_gen);
-      return res.rows;
-    } finally {
-      client.release();
-    }
+    let promise = new Promise((resolve, reject)=>{
+      db.query(query_gen, "", (err, result) => {
+        if (err) {
+          console.error('Error executing query', err.stack);
+          resolve([]);
+        }else{
+          resolve(result.rows);     
+        }     
+      })
+    })    
+    return await promise;   
+
   }catch(err){
     console.log("caught exception ", err)
     return err;
