1d61cc2267b55225984100bd952964599ca09857
Matthew Kelly <matthew.kelly@block.one>
2019-10-10 14:25:47 +0800

    - Updated request_tokens method to accept request limit, action name and requested from params - Updated rpc api TS declaration file & added comment explaining usage of request_tokens

--- packages/api-rpc/src/api/request_tokens.ts
+++ packages/api-rpc/src/api/request_tokens.ts
@@ -1,35 +1,54 @@
-import { Api, JsonRpc, Serialize } from 'eosjs';
+import { Api, JsonRpc } from 'eosjs';
 import JsSignatureProvider from 'eosjs/dist/eosjs-jssig';
 import { TextDecoder, TextEncoder } from 'text-encoding';
 const fetch = require('node-fetch');
 
+/**
+  * request_tokens
+  * Requests {quantity} tokens from account {requested_from}, for account {requested_by}. 
+  * Limited to a request every {limit} minutes.
+  * Only used in testnet for requesting TNT.
+  * @param endpoint
+  * @param private_key
+  * @param actor
+  * @param permission
+  * @param quantity
+  * @param limit
+  * @param action_name
+  * @param requested_from
+  * @param requested_by
+  * @returns request result
+  */
 const request_tokens = async (query: {
   endpoint: string,
   private_key: string,
   actor: string,
   permission: string,
   quantity: any,
+  limit: any,
+  action_name: string,
+  requested_from: string,
   requested_by: string
 }) => {
   try {
-    let { endpoint, private_key, actor, permission, quantity = '1.0000 TNT', requested_by } = query;
-    console.log("request_tokens query ", query)
+    let { endpoint, private_key, actor, permission, limit = 60, quantity = '1.0000 TNT', action_name, requested_from, requested_by } = query;
     const rpc = new JsonRpc(endpoint, { fetch });
     const signatureProvider = new JsSignatureProvider([private_key]);
     const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
-
+    
     const result = await api.transact({
       actions: [{
-        account: 'eosio.token',
-        name: 'transfer',
+        account: requested_from,
+        name: action_name,
         authorization: [{
           actor: actor,
           permission: permission,
         }],
         data: {
-          from: 'eosio',
+          from: requested_from,
           to: requested_by,
           quantity: quantity,
+          limit: limit,
           memo: "Requested Tokens"
         }
       }]
