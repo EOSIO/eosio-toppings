f8dd99233373c2dbcc7be2edc7555796d1ceb078
Matthew Kelly <matthew.kelly@block.one>
2019-10-14 10:52:48 +0800

    Updated request_tokens to use new JsSignatureProvider import

--- packages/api-rpc/dist/api/request_tokens.js
+++ packages/api-rpc/dist/api/request_tokens.js
@@ -34,13 +34,10 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 var _this = this;
 Object.defineProperty(exports, "__esModule", { value: true });
 var eosjs_1 = require("eosjs");
-var eosjs_jssig_1 = __importDefault(require("eosjs/dist/eosjs-jssig"));
+var eosjs_jssig_1 = require("eosjs/dist/eosjs-jssig");
 var text_encoding_1 = require("text-encoding");
 var fetch = require('node-fetch');
 /**
@@ -67,7 +64,7 @@ var request_tokens = function (query) { return __awaiter(_this, void 0, void 0, 
                 _c.trys.push([0, 2, , 3]);
                 endpoint = query.endpoint, private_key = query.private_key, actor = query.actor, permission = query.permission, _a = query.limit, limit = _a === void 0 ? 60 : _a, _b = query.quantity, quantity = _b === void 0 ? '1.0000 TNT' : _b, action_name = query.action_name, requested_from = query.requested_from, requested_by = query.requested_by;
                 rpc = new eosjs_1.JsonRpc(endpoint, { fetch: fetch });
-                signatureProvider = new eosjs_jssig_1.default([private_key]);
+                signatureProvider = new eosjs_jssig_1.JsSignatureProvider([private_key]);
                 api = new eosjs_1.Api({ rpc: rpc, signatureProvider: signatureProvider, textDecoder: new text_encoding_1.TextDecoder(), textEncoder: new text_encoding_1.TextEncoder() });
                 return [4 /*yield*/, api.transact({
                         actions: [{
