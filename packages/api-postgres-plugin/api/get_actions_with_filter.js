fd7c11927e606b592bd87943982673cf28dbf668
varshajnagaraja <varshajn99@gmail.com>
2019-10-17 17:16:34 +0800

    Action filter changes for Sent and Received

--- packages/api-postgres-plugin/api/get_actions_with_filter.js
+++ packages/api-postgres-plugin/api/get_actions_with_filter.js
@@ -3,38 +3,36 @@ const db = require('./db');
 const get_actions_with_filter = async (query) => {
   const { action_filter, account_name, max_rgs, page_size = 100, page_num = 1, show_data_size = "false" } = query;
   const table = `
-    chain.action_trace AS a
-    ${(action_filter === 'signed') ? `
-      LEFT JOIN
-        chain.action_trace_authorization AS b
-      ON
-        a.block_num = b.block_num AND a.transaction_id = b.transaction_id AND a.action_ordinal=b.action_ordinal
-      ` : ''}
-      WHERE
-        a.creator_action_ordinal = 0
-        ${(max_rgs !== undefined) ? `AND a.receipt_global_sequence <= ${max_rgs}` : ''}
-        ${(action_filter === 'contract') ? `AND a.act_account = '${account_name}'` : ''}
-        ${(action_filter === 'signed') ? `AND b.actor = '${account_name}'` : ''}
+    chain.action_trace
+    WHERE  
+      ${(action_filter === 'contract') ? `creator_action_ordinal = 0 AND act_account = '${account_name}'` : ''}
+      ${(action_filter === 'signed') ? `creator_action_ordinal = 0 AND actor = '${account_name}'` : ''}
+      ${(action_filter === 'received') ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_to = '${account_name}'` : ''}
+      ${(action_filter === 'sent') ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_from = '${account_name}'` : ''}
+      ${(max_rgs !== undefined) ? `AND receipt_global_sequence <= ${max_rgs}` : ''}
   `;
 
   const result_statement = `
     SELECT
-      a.transaction_id AS id,
-      a.block_num,
-      a.timestamp,
-      a.act_account,
-      a.act_name,
-      a.receipt_global_sequence
+      transaction_id AS id,
+      block_num,
+      timestamp,
+      act_account,
+      act_name,
+      receipt_global_sequence,
+      token_to,
+      token_from,
+      amount
     FROM
       ${table}
     ORDER BY
-      a.receipt_global_sequence DESC
+      receipt_global_sequence DESC
     LIMIT ${page_size}
     ${(max_rgs !== undefined) ? `OFFSET ${page_size * (page_num - 1)}` : ''}
   `;
 
   const count_statement = `SELECT COUNT(*) AS count FROM ${table}`;
-
+  
   try {
     const data = (await db.queryAsync(result_statement, "")).rows;
     const result = {
