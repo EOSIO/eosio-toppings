0a1ef2a8172ac41529c499d95b4ed9247250cf3c
varshajnagaraja <varshajn99@gmail.com>
2019-12-06 17:44:47 +0800

    Added comments

--- packages/api-postgres-plugin/api/get_permission_link.js
+++ packages/api-postgres-plugin/api/get_permission_link.js
@@ -3,7 +3,7 @@ const db = require('./db');
 const get_permission_link = async (query) => {
   try{
     let { account_name } = query;
-
+    // 'abc' is added here to avoid SQL error
     let query_gen = `
     SELECT account, code as contract, message_type as action_name, required_permission as permission  FROM chain.permission_link
     WHERE ${(account_name !== undefined) ? `account = '${account_name}'`:  'abc' }  `;
