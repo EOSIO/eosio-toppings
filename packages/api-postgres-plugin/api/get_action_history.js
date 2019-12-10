797588bf9bfd639ea9a7af1978eea10e34f52c8b
John Wong <55980051+b1-johnwong@users.noreply.github.com>
2019-12-10 15:48:27 +0800

    optimise action history query (#121)

--- packages/api-postgres-plugin/api/get_action_history.js
+++ packages/api-postgres-plugin/api/get_action_history.js
@@ -3,39 +3,46 @@ const db = require('./db');
 const get_action_history = async (query) => {
   console.log("request received in api-postgres ", new Date());
   try{
-    let { account_name, actor_name, records_count } = query;
+    const { account_name, actor_name, records_count } = query;
 
-    let query_gen = `
-    SELECT transaction_id, action_ordinal, act_account, act_name, act_data, timestamp, block_num, actor, permission
-    FROM chain.action_trace
-    WHERE creator_action_ordinal = 0 
-    ${(account_name !== undefined && actor_name !== undefined) ? 
-      `AND (act_account = ANY('{${account_name}}') OR actor = ANY('{${actor_name}}'))` :
-    (account_name !== undefined) ?
-      `AND act_account = ANY('{${account_name}}')` :
-    (actor_name !== undefined) ? 
-      `AND actor = ANY('{${actor_name}}')` :
-    ``}
-    ORDER BY block_num DESC
-    LIMIT ${(records_count !== undefined) ? parseInt(records_count) :  100}`;
+    const query_gen = `
+      ${account_name !== undefined ?
+        `
+          SELECT transaction_id, action_ordinal, act_account, act_name, act_data, timestamp, block_num, actor, permission
+          FROM chain.action_trace
+          WHERE creator_action_ordinal = 0 AND act_account = ANY('{${account_name}}')
+        ` : ``
+      }
+      ${account_name !== undefined && actor_name !== undefined ? `UNION` : ``}
+      ${actor_name !== undefined ? 
+        `
+          SELECT transaction_id, action_ordinal, act_account, act_name, act_data, timestamp, block_num, actor, permission
+          FROM chain.action_trace
+          WHERE creator_action_ordinal = 0 AND actor = ANY('{${actor_name}}')
+        `  : ``
+      }
+          ORDER BY block_num DESC
+          LIMIT ${records_count !== undefined ? parseInt(records_count) : 100}
+    `;
      
-    console.log("query gen ", query_gen)
+    console.log("query gen ", query_gen);
     
-    let promise = new Promise((resolve, reject)=>{
+    const promise = new Promise((resolve, reject) => {
       db.query(query_gen, "", (err, result) => {
         if (err) {
           console.error('Error executing get action history query: ', err.stack);
           resolve([]);
-        }else{
+        } else {
           console.log("response received from database, sending to back end", new Date());
           resolve(result.rows);     
         }     
-      })
-    })    
+      });
+    });
+
     return await promise;   
 
-  }catch(err){
-    console.log("caught exception ", err)
+  } catch (err) {
+    console.log("caught exception ", err);
     return err;
   }
 }
