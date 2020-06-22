8c77dfe88d0b9b12b0b6ed3e337e1f982c284168
Shupeng Wang <shupeng.wang@block.one>
2020-06-22 17:01:07 +0800

    remove filter by token

--- packages/api-postgres-plugin/api/get_actions_with_filter.js
+++ packages/api-postgres-plugin/api/get_actions_with_filter.js
@@ -12,13 +12,13 @@ const get_actions_with_filter = async query => {
   } = query;
   const limit = Math.min(parseInt(page_size) || 100, 100);
   const table = `
-    chain.action_trace
+    testnet.action_trace
     WHERE
       ${action_filter !== undefined && account_name !== undefined
-        ? action_filter === 'contract' ? `creator_action_ordinal = 0 AND act_account = '${account_name}'` : 
-          action_filter === 'signed' ? `creator_action_ordinal = 0 AND actor = '${account_name}'` : 
-          action_filter === 'received' ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_to = '${account_name}'` : 
-          action_filter === 'sent' ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_from = '${account_name}'` : 
+        ? action_filter === 'contract' ? `creator_action_oridnal = 0 AND act_account = '${account_name}'` : 
+          action_filter === 'signed' ? `creator_action_oridnal = 0 AND actor = '${account_name}'` : 
+          // action_filter === 'received' ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_to = '${account_name}'` : 
+          // action_filter === 'sent' ? `receiver = 'eosio.token' AND act_account = 'eosio.token' AND act_name = 'transfer' AND token_from = '${account_name}'` : 
           'transaction_id IS NOT NULL'
         : 'transaction_id IS NOT NULL'}
       ${max_rgs !== undefined
@@ -41,11 +41,7 @@ const get_actions_with_filter = async query => {
       act_account,
       act_name,
       actor,
-      receipt_global_sequence,
-      token_to,
-      token_from,
-      amount,
-      symbol
+      receipt_global_sequence
     FROM
       ${table}
     ORDER BY
